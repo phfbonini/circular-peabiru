8c20a21982d02d17b7a7c7b2019eae50cf79629d
Pedro Bonini <phf_apps@outlook.com>
2022-08-12 08:43:48 -0300

    FEATURE: MAPS - iniciando a construção dos mini para ver os pontos de parada da circular

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,3 +1,4 @@
+//navbar
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
@@ -42,3 +43,22 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
+
+//
+
+var map;
+
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), { center : {lat:-23.913940, lng:-52.344723}, zoom:15});}
+
+var marker = new google.maps.Marker({
+  position: {lat:-23.912796, lng:-52.349758},
+  map: map,
+  });
+  
+
+function onMap(value){
+  if(value == 1){
+   map.addEventListener
+  }
+}
